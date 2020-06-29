ea0144dc92433a94b5b6dae7ac4c22fd581630e7
yatabis <39yatabis.rim8820xxx@gmail.com>
2020-06-29 18:08:29 +0900

    add: リバーシのロジックの追加

---
+++ reversi/reversi.go
@@ -0,0 +1,23 @@
+package reversi
+
+type Reversi struct {
+	Board
+	turn int
+}
+
+func Init() Reversi {
+	board := initBoard()
+	rv := Reversi{board, 1}
+	return rv
+}
+
+func (rv *Reversi) Put(t, x, y int) bool {
+	if t != rv.turn {
+		return false
+	}
+	if !rv.Board.put(t, x, y) {
+		return false
+	}
+	rv.turn = 3 - rv.turn
+	return true
+}
