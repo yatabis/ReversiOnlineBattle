2966a69f47f9c15c3b9e815778056187444f3160
yatabis <39yatabis.rim8820xxx@gmail.com>
2020-07-10 18:21:08 +0900

    add: ホスト用の対戦相手待機画面を作成

--- client/client.go
+++ client/client.go
@@ -10,27 +10,35 @@ import (
 
 func Init(mux *http.ServeMux) {
 	mux.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("./website/static/"))))
+	mux.Handle("/waiting", http.HandlerFunc(waitingHandler))
 	mux.Handle("/play", http.HandlerFunc(playHandler))
 }
 
-func playHandler(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("./website/static/html/play.html")
+func waitingHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("./website/static/html/waiting.html")
 	if err != nil {
 		log.Println(err)
 		return
 	}
-	if _, err := r.Cookie("GameID"); err != nil {
+	gameId := ""
+	if g, err := r.Cookie("GameID"); err == nil {
+		gameId = g.Value
+	} else {
 		playerId := ""
 		if p, err := r.Cookie("PlayerID"); err == nil {
 			playerId = p.Value
 		}
-		gameId, playerId := websocket.StartGame("", playerId)
+		gameId, playerId = websocket.StartGame("", playerId)
 		http.SetCookie(w, cookie("GameID", gameId))
 		http.SetCookie(w, cookie("PlayerID", playerId))
 	}
-	if err := t.Execute(w, nil); err != nil {
+	if err := t.Execute(w, map[string]string{"GameID": gameId}); err != nil {
 		log.Println(err)
 	}
+}
+
+func playHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./website/static/html/play.html")
 }
 
 func cookie(name, value string) *http.Cookie {
