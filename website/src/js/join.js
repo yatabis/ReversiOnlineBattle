7cd4992514f0772293eedc13c2dd5837b09654b0
yatabis <39yatabis.rim8820xxx@gmail.com>
2020-07-18 18:09:57 +0900

    add: 不正な合戦名を入力したときのエラーメッセージ

--- website/src/js/join.js
+++ website/src/js/join.js
@@ -1,10 +1,16 @@
 const join = () => {
     const gameId = document.getElementById("game-id").value
+    if (!gameId) {
+        document.getElementById("message").innerText = "合戦名を入力してください！"
+        return
+    }
     fetch(location.href + "?gameid=" + gameId)
         .then(result => result.text())
         .then(text => {
             if (text === gameId) {
                 location.href = location.origin + "/play"
+            } else {
+                document.getElementById("message").innerText = `「${gameId}」という合戦はまだ始まっていません！`
             }
         })
 }
